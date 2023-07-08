5c639ec94c973fe45a02efd931e5f19f59c6cec5
DhafinDzahin <dzahindhafin@gmail.com>
2023-07-08 15:18:01 +0800

    feat: add 3 round to the game

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function checkWinner(playerWeapon, computerWeapon) {
-	let options = ["rock", "paper", "scissor"];
+var playerWeapon, playerScore, computerScore, options;
 
+function checkWinner(playerWeapon, computerWeapon) {
 	while (!options.includes(playerWeapon)) {
 		playerWeapon = playerSelectWeapon();
 	}
@@ -10,8 +10,11 @@ function checkWinner(playerWeapon, computerWeapon) {
 		(playerWeapon === "paper" && computerWeapon === "rock") ||
 		(playerWeapon === "rock" && computerWeapon === "scissor") ||
 		(playerWeapon === "paper" && computerWeapon === "rock")
-	)
+	) {
+		playerScore += 1;
 		return "Player win";
+	}
+	computerScore += 1;
 	return "Computer win";
 }
 
@@ -20,10 +23,21 @@ function playerSelectWeapon() {
 	return playerWeapon;
 }
 
-var playerWeapon = playerSelectWeapon();
-let options = ["rock", "paper", "scissor"];
-let computerWeapon = options[Math.floor(Math.random() * options.length)];
+function game() {
+	playerScore = 0;
+	computerScore = 0;
+	while (playerScore < 3 && computerScore < 3) {
+		playerWeapon = playerSelectWeapon();
+		options = ["rock", "paper", "scissor"];
+		let computerWeapon = options[Math.floor(Math.random() * options.length)];
 
-console.log(checkWinner(playerWeapon, computerWeapon));
-console.log(`Player choose ${playerWeapon}`);
-console.log(`Computer choose ${computerWeapon}`);
+		console.log(checkWinner(playerWeapon, computerWeapon));
+		console.log(`Player choose ${playerWeapon}`);
+		console.log(`Computer choose ${computerWeapon}`);
+		console.log(`Player score: ${playerScore} computer score: ${computerScore}`);
+	}
+	if (playerScore === 3) return 'Player is the game winner'
+	return 'Computer is the game winner'
+}
+console.log('first to three win')
+console.log(game());

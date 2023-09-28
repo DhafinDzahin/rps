29703079adb7ad4814b916e565117c4f4516a5c6
DhafinDzahin <dzahindhafin@gmail.com>
2023-09-28 12:09:43 +0800

    add dark mode

--- script.js
+++ script.js
@@ -31,15 +31,15 @@ function checkWinner(playerWeapon, computerWeapon) {
 
 function game(playerWeapon) {
 	// playerWeapon = prompt("Choose a weapon ('rock' 'paper' 'scissor')").toLowerCase();
-	winner.textContent = ''
+	winner.textContent = '';
 	computerWeapon = options[Math.floor(Math.random() * options.length)];
 
 	let result = checkWinner(playerWeapon, computerWeapon);
 	playerPick.src = `./image/${playerWeapon}/${result.player}.jpg`;
 	computerPick.src = `./image/${computerWeapon}/${result.computer}.jpg`;
 
-	playerScorer.textContent = playerScore
-	computerScorer.textContent = computerScore
+	playerScorer.textContent = playerScore;
+	computerScorer.textContent = computerScore;
 	if (playerScore === 5) {
 		winner.textContent = 'Player is the game winner';
 		restart();
@@ -55,6 +55,12 @@ function restart() {
 	computerScore = 0;
 }
 
+function mode(e) {
+	modes.forEach(element => { element.classList.toggle('active'); });
+	if (e.target.id === 'light')  document.body.style.background = '#4f4f4f'
+	else document.body.style.background = '#fff'
+}
+
 playerScore = 0;
 computerScore = 0;
 const options = ["rock", "paper", "scissor"];
@@ -63,16 +69,16 @@ const options = ["rock", "paper", "scissor"];
 // const scissor = document.getElementById("scissor");
 const playerPick = document.getElementById("playerPick");
 const computerPick = document.getElementById("computerPick");
-const option = [...document.getElementById("options").children]
-const playerScorer = document.getElementById("playerScore")
-const computerScorer = document.getElementById("computerScore")
-const winner = document.getElementById('winner')
+const option = [...document.getElementById("options").children];
+const playerScorer = document.getElementById("playerScore");
+const computerScorer = document.getElementById("computerScore");
+const winner = document.getElementById('winner');
+const modes = [...document.getElementsByClassName('mode')];
 
-console.log(option)
 // rock.addEventListener('click', () => game('rock'));
 // paper.addEventListener('click', () => game('paper'));
 // scissor.addEventListener('click', () => game('scissor'));
 
-option.forEach(element => {
-	element.addEventListener('click', () => game(element.id))
-});
+option.forEach(element => element.addEventListener('click', () => game(element.id)));
+
+modes.forEach(element => element.addEventListener('click', (e) => mode(e)));
